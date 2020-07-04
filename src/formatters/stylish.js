e6623d08917276c265a772af46d777867cce5b7f
Anton Terechov <antiy.vt@gmail.com>
2020-07-04 11:06:24 +0200

    code refactoring round 4

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,20 +1,20 @@
 import _ from 'lodash';
 
 const makeIndent = (depth) => {
-  const tabulation = '  ';
-  return tabulation.repeat(1 + depth * 2);
+  const tabulation = '    ';
+  return tabulation.repeat(depth);
 };
 
 const convertObjectToString = (obj, depth) => {
   const keys = Object.keys(obj);
   const eachValueToString = keys.map((key) => {
     if (_.isObject(obj[key]) && !_.isArray(obj[key])) {
-      return `${makeIndent(depth)}  ${key}: ${convertObjectToString(obj[key], depth + 1)}`;
+      return `${makeIndent(depth + 1)}${key}: ${convertObjectToString(obj[key], depth + 1)}`;
     }
-    return `${makeIndent(depth)}  ${key}: ${obj[key]}`;
+    return `${makeIndent(depth + 1)}${key}: ${obj[key]}`;
   });
   const convertedToString = eachValueToString.join('\n');
-  return `{\n${convertedToString}\n  ${makeIndent(depth - 1)}}`;
+  return `{\n${convertedToString}\n${makeIndent(depth)}}`;
 };
 
 const convertValue = (value, depth) => {
@@ -28,21 +28,20 @@ const makeLine = (key, marker, value, depth) => `${makeIndent(depth)}${marker}${
 
 const stylish = (ast, depth = 0) => {
   const render = {
-    nested: (node) => makeLine(node.key, '  ', stylish(node.children, depth + 1), depth),
-    added: (node) => makeLine(node.key, '+ ', convertValue(node.value, depth + 1), depth),
-    removed: (node) => makeLine(node.key, '- ', convertValue(node.value, depth + 1), depth),
-    unchanged: (node) => makeLine(node.key, '  ', convertValue(node.value, depth + 1), depth),
+    nested: (node) => makeLine(node.key, '    ', stylish(node.children, depth + 1), depth),
+    added: (node) => makeLine(node.key, '  + ', convertValue(node.value, depth + 1), depth),
+    removed: (node) => makeLine(node.key, '  - ', convertValue(node.value, depth + 1), depth),
+    unchanged: (node) => makeLine(node.key, '    ', convertValue(node.value, depth + 1), depth),
     changed: (node) => {
-      const renderedRemovedValue = makeLine(node.key, '- ', convertValue(node.oldValue, depth + 1), depth);
-      const renderedAddedValue = makeLine(node.key, '+ ', convertValue(node.newValue, depth + 1), depth);
+      const renderedRemovedValue = makeLine(node.key, '  - ', convertValue(node.oldValue, depth + 1), depth);
+      const renderedAddedValue = makeLine(node.key, '  + ', convertValue(node.newValue, depth + 1), depth);
       return `${renderedRemovedValue}\n${renderedAddedValue}`;
     },
   };
 
   const stringifiedNode = ast.flatMap((node) => render[node.type](node, depth));
   const result = stringifiedNode.join('\n');
-  const makeFinalBracket = (level) => ((level === 0) ? '}' : `${makeIndent(level - 1)}  }`);
-  return `{\n${result}\n${makeFinalBracket(depth)}`;
+  return `{\n${result}\n${makeIndent(depth)}}`;
 };
 
 export default stylish;

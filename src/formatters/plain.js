8594e7bdd0dbb39fee289479f4c948b1c52be679
Anton Terechov <antiy.vt@gmail.com>
2021-01-07 23:09:30 +0100

    code refactoring round 6

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -9,12 +9,14 @@ const pathToProperty = (path, key) => {
   return path.concat('.', key);
 };
 
+const pathToPropertyMessage = (path, nodeKey) => `Property '${pathToProperty(path, nodeKey)}' was`;
+
 const plain = (ast, path = '') => {
   const render = {
     nested: (node) => plain(node.children, pathToProperty(path, node.key)),
-    added: (node) => `Property '${pathToProperty(path, node.key)}' was added with value: ${convertValue(node.value)}`,
-    removed: (node) => `Property '${pathToProperty(path, node.key)}' was deleted`,
-    changed: (node) => `Property '${pathToProperty(path, node.key)}' was changed from ${convertValue(node.oldValue)} to ${convertValue(node.newValue)}`,
+    added: (node) => `${pathToPropertyMessage(path, node.key)} added with value: ${convertValue(node.value)}`,
+    removed: (node) => `${pathToPropertyMessage(path, node.key)} deleted`,
+    changed: (node) => `${pathToPropertyMessage(path, node.key)} changed from ${convertValue(node.oldValue)} to ${convertValue(node.newValue)}`,
     unchanged: () => [],
   };
   return ast.flatMap((node) => render[node.type](node, path)).join('\n');
